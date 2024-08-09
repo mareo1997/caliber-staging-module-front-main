5ebb2224ae31650f889f61df6f93d14498891a22
mareo1997 <50336724+mareo1997@users.noreply.github.com>
2024-08-09 11:52:20 -0400

    Add files via upload

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,17 @@
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
+
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyC4sxZlT-McTildwtxa8LV1lj7ZQhzOrs0',
+    authDomain: 'training-team-253916.firebaseapp.com',
+    projectId: 'training-team-253916',
+    storageBucket: 'training-team-253916.appspot.com',
+    messagingSenderId: '492701958610',
+    appId: '1:492701958610:web:4a30a1be93803701d3480b',
+    measurementId: 'G-DP6XDH9DTW',
+  },
+  CALIBER_URL: `https://caliber2-mock.revaturelabs.com/mock`,
+  BASE_URL: `http://localhost:8081/`
+  // BASE_URL: `http://ec2-54-237-27-253.compute-1.amazonaws.com:8081/`
+};
